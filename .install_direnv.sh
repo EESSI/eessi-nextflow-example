2ed6747af11bee9e072799dc8edaf51a3e79013f
Alan O'Cais <alan.ocais@cecam.org>
2023-05-15 14:35:25 +0200

    Add hidden method to install direnv

---
+++ .install_direnv.sh
@@ -0,0 +1,6 @@
+# Make sure there's a location already in PATH that we can install to
+mkdir -p .local/bin
+# Run the direnv installer (!!not secure!! we will ship direnv with EESSI in 2023.04)
+curl -sfL https://direnv.net/install.sh | bash
+# Add the direnv hook to our shell
+echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc
