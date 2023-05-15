2124832a6eaa6a9457e450adf0c26085ca30132d
Alan O'Cais <alan.ocais@cecam.org>
2023-05-15 14:39:53 +0200

    Tweak direnv install

--- .install_direnv.sh
+++ .install_direnv.sh
@@ -1,6 +1,7 @@
 # Make sure there's a location already in PATH that we can install to
-mkdir -p .local/bin
+mkdir -p $HOME/.local/bin &&
 # Run the direnv installer (!!not secure!! we will ship direnv with EESSI in 2023.04)
-curl -sfL https://direnv.net/install.sh | bash
+curl -sfL https://direnv.net/install.sh | bash &&
 # Add the direnv hook to our shell
-echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc
+echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc &&
+echo "-- Installed direnv! --"
