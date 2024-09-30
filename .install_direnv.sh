57ab83f95909b73df12c8cdf9a3087fdd742586d
ocaisa <alan.ocais@cecam.org>
2024-09-30 14:02:45 +0200

    Revert change

--- .install_direnv.sh
+++ .install_direnv.sh
@@ -1,17 +1,9 @@
 # This can be clunky and invasive, that's why it is hidden
 
-if command -v direnv &> /dev/null
-then
-    echo "direnv command exists, no need for an install"
-else
-    echo "direnv does not exist, attempting an install"
-
-    # Make sure there's a location already in PATH that we can install to
-    mkdir -p $HOME/.local/bin &&
-    # Run the direnv installer (!!not secure!! we should ship direnv with EESSI)
-    curl -sfL https://direnv.net/install.sh | bash &&
-fi
-
+# Make sure there's a location already in PATH that we can install to
+mkdir -p $HOME/.local/bin &&
+# Run the direnv installer (!!not secure!! we should ship direnv with EESSI)
+curl -sfL https://direnv.net/install.sh | bash &&
 # Add the direnv hook to our shell
 echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc &&
 eval "$(direnv hook bash)"
