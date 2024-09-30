b778dd9d0b65571213a506fc1588a8f7976150af
ocaisa <alan.ocais@cecam.org>
2024-09-30 13:56:07 +0200

    Allow for fact that direnv may exists

--- .install_direnv.sh
+++ .install_direnv.sh
@@ -1,13 +1,21 @@
-# This is clunky and invasive, that's why it is hidden
+# This can be clunky and invasive, that's why it is hidden
 
-# Make sure there's a location already in PATH that we can install to
-mkdir -p $HOME/.local/bin &&
-# Run the direnv installer (!!not secure!! we should ship direnv with EESSI)
-curl -sfL https://direnv.net/install.sh | bash &&
+if command -v direnv &> /dev/null
+then
+    echo "direnv command exists, no need for an install"
+else
+    echo "direnv does not exist, attempting an install"
+
+    # Make sure there's a location already in PATH that we can install to
+    mkdir -p $HOME/.local/bin &&
+    # Run the direnv installer (!!not secure!! we should ship direnv with EESSI)
+    curl -sfL https://direnv.net/install.sh | bash &&
+fi
+
 # Add the direnv hook to our shell
 echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc &&
 eval "$(direnv hook bash)"
 echo ""
-echo "-----------------------"
-echo "-- Installed direnv! --"
-echo "-----------------------"
+echo "--------------------------------------"
+echo "-- direnv available and configured! --"
+echo "--------------------------------------"
